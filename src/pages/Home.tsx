724c8904490dbf5a5694ac09adfcd7923a84afd4
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-08-04 09:46:44 -0700

    Add additional info to about me

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,8 @@ import {
 // import KevinPic from '../assets/kevin-pic.png';
 
 function Home() {
-  const space = 2;
-  // Color backgrounds
+  const space = 3;
   const card = useColorModeValue('whiteAlpha.600', 'blackAlpha.600');
-  // const card = useColorModeValue('gray.100', 'gray.800');
   const subCard = useColorModeValue('gray.200', 'gray.700');
   const borderColor = useColorModeValue('gray.300', 'gray.600');
 
@@ -36,10 +34,26 @@ function Home() {
       >
         <Heading display="flex" justifyContent="center" marginBottom={space}>About Me</Heading>
         <p>Hello! I&apos;m Kevin Hernandez.</p>
-        <p>This is a simple portfolio site using React Typescript and Chakra UI.</p>
+        <p>Made with React Typescript and Chakra UI.</p>
         <p>Recent UC Berkeley Extension Graduate</p>
+        <p>Allan Hancock Computer Science</p>
         {/* <Image position="absolute"
         right={space} bottom={space} borderRadius="50%" maxWidth={256} src={KevinPic} /> */}
+      </Box>
+      <Box
+        margin={space}
+        padding={space}
+        bg={subCard}
+        rounded="lg"
+        borderStyle="solid"
+        borderWidth="thin"
+        borderColor={borderColor}
+      >
+        <p>
+          Currently working part time at 2U as a Web Instructional Specialist on MERN stack.
+          I&apos;m currently persuing a frontend position.
+          Feel free to checkout my projects and reach out to me, lets see if we can work together!
+        </p>
       </Box>
     </Box>
   );
