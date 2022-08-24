d2e07721c0ce2197777a34472095b01dce8f8099
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-08-24 10:14:03 -0700

    Add mapping of skilllist component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { GridItem, Center, useColorModeValue } from '@chakra-ui/react';
+import { v4 as uuidv4 } from 'uuid';
 
 type Props = {
   skillList: Array<string>;
 };
 
-export default function Skills({}: Props) {
-  return <div>Skills</div>;
+export default function Skills({ skillList }: Props) {
+  const spacing = 3;
+  const borderColor = useColorModeValue('gray.300', 'gray.600');
+  const card = useColorModeValue('whiteAlpha.600', 'blackAlpha.600');
+  return (
+    <GridItem
+      colSpan={1}
+      bg={card}
+      rounded="md"
+      padding={spacing}
+      border="solid 1px"
+      borderColor={borderColor}
+    >
+      <Center>
+        <ul>
+          {skillList.map((skill, i) => (
+            <li key={uuidv4()}>{skill}</li>
+          ))}
+        </ul>
+      </Center>
+    </GridItem>
+  );
 }
